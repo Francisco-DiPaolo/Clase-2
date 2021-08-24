7d173b7bcdf5b20acca7601ee74cf982d3add213
Francisco Di Paolo <franciscogermandipaolo@gmail.com>
2021-08-24 20:09:09 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prog_2_Prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
