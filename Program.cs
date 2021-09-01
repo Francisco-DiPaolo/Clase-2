be2f786459efe2e844a1f5e1901c90952f433f58
Francisco Di Paolo <franciscogermandipaolo@gmail.com>
2021-08-31 21:17:21 -0300

    Clase3

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using Jugadores;
 
-namespace Prog_2_Prueba
+namespace Clase2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("New World!");
+           Jugador jug1 = new Jugador("Pepe", 30, true); 
+           Jugador jug2 = new Jugador("Juan", 20);
+           Jugador jug3 = new Jugador("Dante", 15, true);
+
+           Console.WriteLine(jug2.IsActivo());
         }
     }
 }
